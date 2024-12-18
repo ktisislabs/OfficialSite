fc7d0389e27e4a82b2d5b2fbe4595af4f2d39602
adixb <aditya.bhattacharjee706@gmail.com>
2024-12-18 18:15:50 +0530

    updated code

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,8 +4,7 @@ import { TestimonialsData } from '../Data/testimonials';
 function Testimonials() {
   return (
     <>
-      <section className="Testimonials-section w-full h-auto bg-black py-12 mt-36"  >
-      
+      <section className="Testimonials-section w-full h-auto bg-black py-12 mt-36">
         <div className="container mx-auto px-4">
           <h2 className="text-4xl font-semibold font-bold text-center text-white mb-8">
             What Our Clients Say
@@ -14,7 +13,7 @@ function Testimonials() {
             {TestimonialsData.map((item) => (
               <div
                 key={item.id}
-                className="testimonial-card bg-black text-white p-6 rounded-lg  border shadow hover:shadow-xl transition-shadow"
+                className="testimonial-card bg-black text-white p-6 rounded-lg border shadow hover:shadow-xl transition-shadow"
               >
                 <div className="flex items-center mb-4">
                   <img
@@ -26,6 +25,15 @@ function Testimonials() {
                     <h3 className="text-lg font-semibold">{item.Name}</h3>
                     <p className="text-sm text-gray-400">{item.Designation || "Valued Client"}</p>
                   </div>
+                </div>
+                <div className="flex items-center mb-4">
+                  {Array(5)
+                    .fill(0)
+                    .map((_, index) => (
+                      <span key={index} className="text-yellow-400 text-lg">
+                        ★
+                      </span>
+                    ))}
                 </div>
                 <p className="text-gray-300 italic">"{item.Remark}"</p>
               </div>
